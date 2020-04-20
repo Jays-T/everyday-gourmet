d3e46edb291d188ba5e14a9758c4d53925c4b9fd
Jays-T <j.j.h.traas@gmail.com>
2020-04-20 19:37:54 +0000

    added the fade in headlines using AOS Animate On Scroll by Michał Sajnóg

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,3 +1,5 @@
+/* Solution created by Dan Wilson and sourced from https://codepen.io/danwilson/pen/dqZvmx */
+
 const selects = Array.from(document.querySelectorAll('select'));
 const display = document.querySelector('.display');
 
