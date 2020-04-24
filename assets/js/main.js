994604256e90998ab115721fe133528a65ad9c98
Jays-T <j.j.h.traas@gmail.com>
2020-04-24 15:41:16 +0000

    removed unused smooth scroll code from main.js / adjusted the margins on fade in headlines

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,13 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
 
 // Front page animated arrow scroll function 
+
 $(".arrow").click(function() {
   $("#headline-one")[0].scrollIntoView({
     behavior: "smooth", 
     block: "center" 
 });
 })
+
 
 // Spoonacular API xml request
 
@@ -40,7 +18,7 @@ xhr.withCredentials = true;
 xhr.addEventListener("readystatechange", function () {
 	if (this.readyState === this.DONE) {
         document.getElementById("data").innerHTML = this.responseText;
-		console.log(this.responseText);
+		console.log(JSON.parse(this.responseText));
 	}
 });
 
