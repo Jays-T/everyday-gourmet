6b0e1b082673e3ba6e501c5f67d53d8efd8fe974
Jays-T <j.j.h.traas@gmail.com>
2020-04-24 14:45:31 +0000

    added XML req + Event Listener / tested and working / code returns : no endpoint error

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,26 @@ $(".arrow").click(function() {
 });
 })
 
+// Spoonacular API xml request
+
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+        document.getElementById("data").innerHTML = this.responseText;
+		console.log(this.responseText);
+	}
+});
+
+xhr.open("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
+//xhr.open("GET", baseURL + food + '=' + $document.getElementById("inputMeal");
+xhr.setRequestHeader("x-rapidapi-host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "556f1a08acmsh72d41478bea3e6ap19227ajsn0bf8a497994a");
+
+xhr.send();
+
+
+
+
