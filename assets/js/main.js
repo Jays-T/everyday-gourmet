123804fb71e1ff3d51117778cbcb90a8d46c4e25
Jays-T <j.j.h.traas@gmail.com>
2020-04-25 21:40:53 +0000

    changed input placeholder text / tested spoonacular API functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,31 @@ $(".arrow").click(function() {
     });
 })
 
+// XMLRequest for spoonacular API  
+
+var data = null;
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open("GET", "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/pairing?maxPrice=50&food=fish");
+xhr.setRequestHeader("x-rapidapi-host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "556f1a08acmsh72d41478bea3e6ap19227ajsn0bf8a497994a");
+
+xhr.send(data);
+
+// 
+
+const Url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/pairing?";
+
+$('#get-wine-sug').click(function(xhr) {
+    $.get(Url, function(data,status){
+        console.log(`$data`);
+    });
+});
